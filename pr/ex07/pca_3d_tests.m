1538f071a474840dc8bb5a3010bf818ff72a29cd
dominik.neumann <dominik.neumann@gmail.com>
2010-11-11 20:31:44 +0000

    += pca generalized

--- pr/ex07/pca_3d_tests.m
+++ pr/ex07/pca_3d_tests.m
@@ -4,17 +4,14 @@ clear all
 clc
 
 %% params
-dim = 50;
+dim = 3;
 n_features = 2500;
 
-%% feature matrix
-f = rand(dim,n_features) + ones(dim,n_features)*22;
-
-for i = 1:47
-    i
-    f = pca_dimensionality_reduction(f);
-end;
-
+%% shift around 0-vector
+f = [0 0 0; 0 1 1; 2 1 1; -2 -1 3; 1 0 -1]';
+%f = randn(dim, n_features) + ones(dim,n_features)*5;
+f_mean = mean(f,2);
+f = f - repmat(f_mean,1,size(f,2));
 
 %scatter3(f(1,:),f(2,:),f(3,:),'x');
 %axis([-5 5 -5 5 -5 5]);

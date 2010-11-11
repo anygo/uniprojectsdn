0132ff9e92a3d184c43dc8b122331c0857719037
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-11-11 15:18:52 +0000

    advpt 5 u 6

--- advpt/ex02/main5.cpp
+++ advpt/ex02/main5.cpp
@@ -11,4 +11,41 @@
 //=================================================================================================
 
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main(int argc, char* argv[])
+{
+		int *arr = new int[10];
+		for (int i = 0; i < 10; i++)
+				arr[i] = i*i*3;
+
+		int *arr2 = new int[10];
+		copy(arr, arr+10, arr2);
+
+		for (int i = 0; i < 10; i++)
+				if (arr[i] != arr2[i]) 
+						std::cout << i << "te Stelle verschieden" << std::endl;
+
+
+		std::cout << "Original: ";
+		for (int i = 0; i < 10; i++)
+				std::cout << arr[i]  << " ";
+		std::cout << std::endl;
+
+		std::cout << "Arr2: ";
+		for (int i = 0; i < 10; i++)
+				std::cout << arr2[i] << " ";
+		std::cout << std::endl;
+
+
+
+
+		delete arr2;
+		delete arr;
+
+		return 0;
+}
--- advpt/ex02/main6.cpp
+++ advpt/ex02/main6.cpp
@@ -9,4 +9,36 @@
 //=================================================================================================
 
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main(int argc, char* argv[])
+{
+		vector<int> v0;
+		for (int i = 0; i < 10; i++)
+				v0.push_back(i*i*i*i);
+
+		vector<int> v1(v0);
+
+
+		for (int i = 0; i < 10; i++)
+				if (v0[i] != v1[i]) 
+						std::cout << i << "te Stelle verschieden" << std::endl;
+
+
+		std::cout << "Original: ";
+		for (int i = 0; i < 10; i++)
+				std::cout << v0[i]  << " ";
+		std::cout << std::endl;
+
+		std::cout << "Kopie:    ";
+		for (int i = 0; i < 10; i++)
+				std::cout << v1[i] << " ";
+		std::cout << std::endl;
+
+
+		return 0;
+}

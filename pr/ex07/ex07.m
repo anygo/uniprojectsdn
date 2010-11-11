8ed4ed3d610bf1b0bb592795014929b8291e583a
dominik.neumann@gmail.com <dominik.neumann@gmail.com@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-11-11 18:57:18 +0000

    3d pca

--- pr/ex07/ex07.m
+++ pr/ex07/ex07.m
@@ -1,11 +1,11 @@
 %% cleanup
-%close all
+close all
 clear all
 clc
 
 %% params
-dim = 2;
-n_features = 200;
+dim = 3;
+n_features = 250;
 
 %% shift around 0-vector
 %f = [0 0; 0 1; 2 1; -2 -1; 0 -1]';
@@ -13,28 +13,31 @@ f = randn(dim, n_features) + ones(dim,n_features)*5;
 f_mean = mean(f,2);
 f = f - repmat(f_mean,1,n_features);
 
-scatter(f(1,:),f(2,:),'x');
-%axis([-3 3 -3 3]);
+%scatter3(f(1,:),f(2,:),f(3,:),'x');
+%axis([-5 5 -5 5 -5 5]);
+
 hold on;
 [U S V] = svd(f);
 
-
-mu = mean(f,2);
-%mu = [0;0];
-plot([mu(1) mu(1)+U(1,1)*sqrt(S(1,1))], [mu(2) mu(2)+U(1,2)*sqrt(S(1,1))],'r');
-plot([mu(1) mu(1)+U(2,1)*sqrt(S(2,2))], [mu(2) mu(2)+U(2,2)*sqrt(S(2,2))],'r');
-
+for i = 1:dim
+    plot3([0 U(1,i)*sqrt(S(i,i))],[0 U(2,i)*sqrt(S(i,i))],[0 U(3,i)*sqrt(S(i,i))],'r');
+end
 
 for i = 1:size(f,2)
-    
     pt = U(:,1)'*f(:,i)*U(:,1);
-    plot([f(1,i) pt(1)],[f(2,i) pt(2)]);
-    scatter(pt(1),pt(2),'x');
-    
-    pt = U(:,1)'*f(:,i);
-    plot([f(1,i) pt(1)],[f(2,i) 0],'d');
-    scatter(pt(1),10,'o','black');
-    
+    pt = pt + U(:,2)'*f(:,i)*U(:,2);
+    %plot3([f(1,i) pt(1)],[f(2,i) pt(2)],[f(3,i) pt(3)]);
+    scatter3(pt(1),pt(2),pt(3),'.','magenta');
+end
+hold off;
+pause;
 
+cla;
+hold on;
+for i = 1:size(f,2)
+    pt = U(:,1:2)'*f(:,i);
+   % pt = pt + U(:,2)'*f(:,i);
+    %plot3([f(1,i) pt(1)],[f(2,i) pt(2)],[f(3,i) pt(3)]);
+    scatter(pt(1),pt(2),'.','magenta');
 end
 hold off;
---
+++ pr/ex07/pca_2d.m
@@ -0,0 +1,40 @@
+%% cleanup
+close all
+clear all
+clc
+
+%% params
+dim = 2;
+n_features = 200;
+
+%% shift around 0-vector
+%f = [0 0; 0 1; 2 1; -2 -1; 0 -1]';
+f = rand(dim, n_features) + ones(dim,n_features)*5;
+f_mean = mean(f,2);
+f = f - repmat(f_mean,1,n_features);
+
+scatter(f(1,:),f(2,:),'x');
+%axis([-3 3 -3 3]);
+hold on;
+[U S V] = svd(f);
+
+
+mu = mean(f,2);
+%mu = [0;0];
+plot([mu(1) mu(1)+U(1,1)*sqrt(S(1,1))], [mu(2) mu(2)+U(1,2)*sqrt(S(1,1))],'r');
+plot([mu(1) mu(1)+U(2,1)*sqrt(S(2,2))], [mu(2) mu(2)+U(2,2)*sqrt(S(2,2))],'r');
+
+
+for i = 1:size(f,2)
+    
+    pt = U(:,1)'*f(:,i)*U(:,1);
+    plot([f(1,i) pt(1)],[f(2,i) pt(2)]);
+    scatter(pt(1),pt(2),'X');
+    
+    pt = U(:,1)'*f(:,i);
+    plot([f(1,i) pt(1)],[f(2,i) 0],'d');
+    scatter(pt(1),2,'x','black');
+    
+
+end
+hold off;

5fca9d270f4205b48de716c7af4d74feac9b3de9
dominik.neumann <dominik.neumann@gmail.com>
2010-11-22 20:09:18 +0000

    += fisher transform modified.. correct?

--- pr/toolbox/FisherTransform.m
+++ pr/toolbox/FisherTransform.m
@@ -1,36 +1,30 @@
 function [patterns, train_targets, phi] = FisherTransform(train_patterns, train_targets, param, plot_on)
-
 % Task: 
 % Implement a transform that maximizes the inter-class and minimized the 
 % intra-class distance. The transform is then returned as an output
 % argument, together with the transformed patterns and the corresponding
 % train_targets.
 
-Uclasses = unique(train_targets);
 
 [Dim,N] = size(train_patterns);
 
-% initial transformation
-phi = eye(Dim, Dim);
-
-
-% compute interclass kernel matrix 
-mu_joint = mean(train_patterns,2);
-E_inter = zeros(Dim, Dim);
-for i = 1:N
-    x = train_patterns(:,i);
-    E_inter = E_inter + (x-mu_joint)*(x-mu_joint)';
-end
-E_inter = E_inter / N;
-
-% Hint: This is the same matrix as used in the LDA classification!
-% you will need the mean of all class means
-% compute intraclass kernel matrix E_intra
 class0 = train_targets == 0;
 class1 = train_targets == 1;
 
 mu0 = mean(train_patterns(:, class0),2);
 mu1 = mean(train_patterns(:, class1),2);
+mu_joint = mean(train_patterns,2);
+
+% initial transformation
+phi = eye(Dim, Dim);
+
+% compute interclass kernel matrix 
+E_inter = ((mu0-mu_joint)*(mu0-mu_joint)' + (mu1-mu_joint)*(mu1-mu_joint)')/2;
+
+% Hint: This is the same matrix as used in the LDA classification!
+% you will need the mean of all class means
+% compute intraclass kernel matrix E_intra
+
 
 E_intra = zeros(Dim, Dim);
 for i = 1:N
@@ -41,16 +35,18 @@ for i = 1:N
         E_intra = E_intra + (x-mu1)*(x-mu1)';
     end
 end
+E_intra = E_intra / N;
 
 % create the combined kernel matrix E
 E = inv(E_intra)*E_inter;
 
 % solve the eigenvalue / eigenvector problem for E
 [U S V] = svd(E);
-a = U(1,:);
+a = U(:,1);
 
 % create phi such that it maximizes the Rayleigh ratio
-phi = eye(Dim,Dim)*a;
+phi(1,1) = abs(a(1));
+phi(2,2) = abs(a(2));
 
 % apply feature transform
 patterns = phi * train_patterns;

34ac1a049caae8192128dced8895875d5c0e13c6
dominik.neumann@gmail.com <dominik.neumann@gmail.com@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-11-24 18:25:03 +0000

    intropr ex10 .. crap.. :(

--- intropr/ex10/ex10.m
+++ intropr/ex10/ex10.m
@@ -1,18 +1,53 @@
-%% load image
+function ex10()
+close all;
+clear all;
+clc;
+
 I = imread('object.png');
 
-[counts x] = imhist(I,128);
-%counts = counts/sum(counts);
+bins = 128;
+[counts x] = imhist(I,bins);
 
-thresh = x(1);
-thresh = 20000;
+totalSquaredError = zeros(bins,1);
 
+for theta = 2:bins-1
+    
+    % f <= theta
+    vec_left = [];
+    for i = 1:theta
+        vec_left = [vec_left; repmat(x(i),counts(i),1)];
+    end
+    left = mle(vec_left);
+    squaredErrorLeft = 0;
+    
+    if sum(counts(1:theta)) > 0
+        for i = 1:theta
+            squaredErrorLeft = squaredErrorLeft + (eval_gauss(x(i), left(1), left(2)) - counts(i)/sum(counts(1:theta)))^2;
+        end
+    else
+        totalSquaredError(theta) = -1;
+    end
+    
+    
+    
+    % f > theta
+    vec_right = [];
+    for i = theta+1:bins
+        vec_right = [vec_right; repmat(x(i),counts(i),1)];
+    end
+    right = mle(vec_right);
+    squaredErrorRight = 0;
+    
+    if sum(counts(theta+1:bins)) > 0
+        for i = theta+1:bins
+            squaredErrorRight = squaredErrorRight + (eval_gauss(x(i), right(1), right(2)) - counts(i)/sum(counts(theta+1:bins)))^2
+        end
 
-vec = [];
-for i = 65536/thresh:size(x,1)
-   for j = 1:counts(i)
-      vec = [vec; x(i)]; 
-   end
+        totalSquaredError(theta) = squaredErrorLeft + squaredErrorRight;
+    else
+        totalSquaredError(theta) = -1;
+    end
+    
+   
 end
-
-phat = mle(vec);
+end

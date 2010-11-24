6882b646d61ae75f7fd4f9f2e8921f2c0f71aa16
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-11-24 14:56:05 +0000

    intropr..

--- intropr/ex10/ex10.m
+++ intropr/ex10/ex10.m
@@ -1,2 +1,18 @@
 %% load image
 I = imread('object.png');
+
+[counts x] = imhist(I,128);
+%counts = counts/sum(counts);
+
+thresh = x(1);
+thresh = 20000;
+
+
+vec = [];
+for i = 65536/thresh:size(x,1)
+   for j = 1:counts(i)
+      vec = [vec; x(i)]; 
+   end
+end
+
+phat = mle(vec);

f69141720497524fbe22138c0017e8f8164b22f7
dominik.neumann <dominik.neumann@gmail.com>
2010-11-27 13:32:54 +0000

    ...

--- ddwuegw/approach2.m
+++ ddwuegw/approach2.m
@@ -6,19 +6,19 @@ clc;
 %% bild laden    
 img = rgb2gray(imread('bild_sb/b (9).jpg'));
 imagesc(img);
-%pause(0.5);
+pause(0.2);
 img = imfilter(img, fspecial('gaussian', [25 25]));
 imagesc(img);
-%pause(0.5);
+pause(0.2);
 laplacian = imfilter(img, fspecial('laplacian'), 'replicate');
 imagesc(laplacian);
-%pause(0.5);
+pause(0.2);
 
 sigma = 2;
 hsize = [5 5];
 img2 = imfilter(img, fspecial('log', hsize, sigma));
 imagesc(img2);
-%pause(0.5);
+pause(0.2);
 
 %% thresholding
 imgbw = im2bw(img2, graythresh(img2)); 
@@ -27,10 +27,12 @@ imgbw = im2bw(img2, graythresh(img2));
 %imgbw = imdilate(imgbw, strel('disk', 5, 0));
 imagesc(imgbw);
 drawnow;
+pause(0.2);
 
 imgbw = imclose(imgbw, strel('disk', 12));
 imagesc(imgbw);
 drawnow;
+pause(0.2);
 
 imgbw = imopen(imgbw, strel('disk', 45));
 imagesc(img.*uint8(~imgbw))

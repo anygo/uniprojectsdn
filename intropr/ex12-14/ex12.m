bea67e9a9408be95700705f6c6607a157769d053
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-11-28 15:44:12 +0000

    intropr reloaded

--- intropr/ex12-14/ex12.m
+++ intropr/ex12-14/ex12.m
@@ -5,10 +5,24 @@ clc;
 
 %% ex12
 I = imread('moon.jpg');
+I = imfilter(I, fspecial('gaussian'));
+
 mask = [1 1 1; 1 -8 1; 1 1 1];
-filtered = imfilter(I,mask,'replicate');
+filtered = uint8(imfilter(double(I),mask,'replicate'));
 enhanced = I - filtered;
-subplot(1,3,1); imagesc(I); axis off;
-subplot(1,3,2); imagesc(filtered); axis off;
-subplot(1,3,3); imagesc(enhanced); axis off;
+subplot(2,3,1); imagesc(I); axis off, title('orig');
+subplot(2,3,2); imagesc(filtered); axis off, title('laplacian');
+subplot(2,3,3); imagesc(enhanced); axis off, title('enhanced');
+colormap gray;
+
+
+% oder mit sobel:
+dx = imfilter(I,fspecial('sobel'),'replicate');
+dy = imfilter(I,fspecial('sobel')','replicate');
+filtered2 = sqrt(double(dx).^2+double(dy).^2);
+
+enhanced = I - uint8(filtered2);
+subplot(2,3,4); imagesc(I); axis off, title('orig');
+subplot(2,3,5); imagesc(filtered2); axis off, title('sobel');
+subplot(2,3,6); imagesc(enhanced); axis off, title('enhanced');
 colormap gray;

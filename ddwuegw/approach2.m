9c2d998eda59ed55cf08f587fb4ae42a4a716aea
dominik.neumann <dominik.neumann@gmail.com>
2010-11-29 18:00:28 +0000

    += changed to philips version

--- ddwuegw/approach2.m
+++ ddwuegw/approach2.m
@@ -7,7 +7,8 @@ figure(1);
 colormap gray;
     
 %% bild laden    
-img = rgb2gray(imread('bild_sb/b (9).jpg'));
+img = rgb2gray(imread('BildSerie/b (9).jpg'));
+img_org = img;
 imagesc(img);
 pause(pausetime);
 img = imfilter(img, fspecial('gaussian', [25 25]));
@@ -33,24 +34,65 @@ pause(pausetime);
 imgbw = imclose(imgbw, strel('disk', 12));
 subplot(2,1,1), imagesc(img + uint8(~imgbw)*100), subplot(2,1,2), imagesc(imgbw);
 drawnow;
-pause(pausetime);
+%pause(pausetime);
 
 imgbw = imopen(imgbw, strel('disk', 50));
 subplot(2,1,1), imagesc(img + uint8(~imgbw)*100), subplot(2,1,2), imagesc(imgbw);
 drawnow;
-pause(pausetime);
+%pause(pausetime);
 
 imgbw = imclose(imgbw, strel('disk', 50, 0));
 subplot(2,1,1), imagesc(img + uint8(~imgbw)*100), subplot(2,1,2), imagesc(imgbw);
 drawnow;
-pause(pausetime);
+%pause(pausetime);
 
 
-%% kleine zellen finden
-segmented = img .* uint8(imgbw);
-figure(2);
-imagesc(histeq(segmented));
-title('krypten ausgeschnitten');
-colormap gray;
+%% kleine zellen finden 1
+% segmented = img .* uint8(imgbw);
+% figure(3);
+% imagesc(histeq(segmented));
+% title('krypten ausgeschnitten');
+% colormap gray;
 
+%% kleine zelle finden 2
+
+load('kernel.mat');
+%
+%Ergebniss wird zZ nicht weiter verwende, obwohl es "human optical"
+%vielversprechend aussieht
+image_filtered_sharp = imfilter(img,fspecial('unsharp')); 
+
+%groﬂer (25*25) Log mit kleinem Sigma
+image_filtered = imfilter(img,fspecial('log', 25, 0.4));
+%oder
+%image_filtered = imfilter(adapthisteq(img_org),fspecial('log', 25, 0.4));
+
+image_filtered(imgbw==0) = 0;
+
+figure(4)
+imagesc(image_filtered_sharp)
+colormap gray
+figure(5)
+imagesc(image_filtered)
+colormap gray
+
+
+
+
+
+
+%% region growing
+% figure(5)
+% imagesc(imgbw);
+% colormap gray
+% [x_p,y_p] = ginput(1);
+% 
+% 
+% 
+% for i = 1:size(x_p,1)
+% I = im2double((img)); 
+% J = regiongrowing(I,round(x_p(i)),round(y_p(i)),0.1); 
+% figure(4)
+% imagesc(J)
+% end
 

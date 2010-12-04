b4b23dc3109d1d85f731da4f0e93a6a7afdc7e14
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-12-04 17:49:10 +0000

    += minor modifications

--- ddwuegw/approach2.m
+++ ddwuegw/approach2.m
@@ -2,7 +2,6 @@
 clear all;
 close all;
 clc;
-pausetime = 0.01;
 figure(1);
 colormap gray;
     
@@ -10,41 +9,33 @@ colormap gray;
 img = rgb2gray(imread('BildSerie/b (9).jpg'));
 img_org = img;
 imagesc(img);
-pause(pausetime);
 img = imfilter(img, fspecial('gaussian', [25 25]));
 imagesc(img);
-pause(pausetime);
 laplacian = imfilter(img, fspecial('laplacian'), 'replicate');
 imagesc(laplacian);
-pause(pausetime);
 
 sigma = 2;
 hsize = [5 5];
 img2 = imfilter(img, fspecial('log', hsize, sigma));
 imagesc(img2);
-pause(pausetime);
 
 %% thresholding
 imgbw = im2bw(img2, graythresh(img2)); 
 subplot(2,1,1), imagesc(img + uint8(~imgbw)*100), subplot(2,1,2), imagesc(imgbw);
 drawnow;
-pause(pausetime);
 
 %% wildes morphing
 imgbw = imclose(imgbw, strel('disk', 12));
 subplot(2,1,1), imagesc(img + uint8(~imgbw)*100), subplot(2,1,2), imagesc(imgbw);
 drawnow;
-%pause(pausetime);
 
 imgbw = imopen(imgbw, strel('disk', 50));
 subplot(2,1,1), imagesc(img + uint8(~imgbw)*100), subplot(2,1,2), imagesc(imgbw);
 drawnow;
-%pause(pausetime);
 
 imgbw = imclose(imgbw, strel('disk', 50, 0));
 subplot(2,1,1), imagesc(img + uint8(~imgbw)*100), subplot(2,1,2), imagesc(imgbw);
 drawnow;
-%pause(pausetime);
 
 
 %% kleine zellen finden 1

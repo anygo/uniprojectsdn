e2343200b2e87fb241d607f9932f44e6c652f35e
dominik.neumann@gmail.com <dominik.neumann@gmail.com@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-12-12 21:21:05 +0000

    += SVM classifier (hard-margin)

--- pr/toolbox/SVM.m
+++ pr/toolbox/SVM.m
@@ -1,25 +1,37 @@
-function test_targets = NearestNeighbor(train_patterns, train_targets, test_patterns, params)
+function test_targets = SVM(train_patterns, train_targets, test_patterns, params)
+    %% rerepresent classes for Rosenblatts Perceptron {0; 1} -> {-1; 1}
+    train_targets(train_targets == 0) = -1;
 
-% Classify using the Nearest neighbor algorithm
-% Inputs:
-% 	train_patterns	- Train patterns
-%	train_targets	- Train targets
-%   test_patterns   - Test  patterns
-%	params		    - unused
-%
-% Outputs
-%	test_targets	- Predicted targets
+    %% create linear inequations  Ax <= b
+    % x = [alpha_0; alpha(0); alpha(1)]
+    A = zeros(size(train_patterns, 2), 3);
+    b = -ones(size(train_patterns, 2), 1);
+    for i = 1:size(train_patterns, 2)
+        A(i, 1) = -train_targets(1, i);
+        A(i, 2) = -train_targets(1, i) * train_patterns(1, i);
+        A(i, 3) = -train_targets(1, i) * train_patterns(2, i);
+    end
 
-L			= length(train_targets);
-Uc          = unique(train_targets);
-
-N               = size(test_patterns, 2);
-test_targets    = zeros(1,N); 
-for i = 1:N,
-    dist            = sum((train_patterns - test_patterns(:,i)*ones(1,L)).^2);
-    [m, indices]    = sort(dist);
+    %% find minimum
+    x0 = [0; 0; 1];
+    alpha_vec = fmincon(@obj_func, x0, A, b);
     
-    best            = train_targets(indices(1));
+    alpha_0 = alpha_vec(1);
+    alpha = alpha_vec(2:3);
     
-    test_targets(i) = best;
+    %% assign classes
+    test_targets = zeros(1,size(test_patterns,2));
+    for i = 1:size(test_patterns,2)
+        x = test_patterns(:,i);
+        test_targets(1,i) = sign(alpha' * x + alpha_0);
+    end
+    
+    %% rererepresent classes for the classifier-toolbox {-1; 1} -> {0; 1}  
+    test_targets(test_targets == -1) = 0;
+end
+
+% alpha is [alpha_0; alpha(0); alpha(1)] (siehe Übungsblatt (selbst
+% geschriebenes zeug))
+function val = obj_func(alpha)
+    val = norm(alpha(2:3), 2)^2 / 2;
 end

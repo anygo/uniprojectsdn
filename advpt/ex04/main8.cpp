3f3bd8cc9778c4042056321188694586cc5a29d7
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-12-13 16:35:53 +0000

    += advpt main8

--- advpt/ex04/main8.cpp
+++ advpt/ex04/main8.cpp
@@ -9,5 +9,33 @@
 //
 //=================================================================================================
 
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+bool isEven(int i) { return (i%2) == 0; }
+
+int main(int argc, char* argv[]) {
+		std::set<int> myset;
+
+		std::set<int>::iterator it;
+
+		for (int i = 0; i < 12; ++i) {
+				int num = std::rand();
+				myset.insert(num);	
+				std::cout << num << " inserted" << std::endl;
+		}		
+		std::cout << std::endl;
+
+		for (it = myset.begin(); it != myset.end();) {
+				std::set<int>::iterator here = it++;
+				if (isEven(*here))
+						myset.erase(here);
+		}
 
 
+		for (it = myset.begin(); it != myset.end(); ++it) {
+				std::cout << *it << std::endl;
+		}
+}

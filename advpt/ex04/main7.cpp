8ba8ab214b9529378f6af4034a8f60cd65eeb52d
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-12-13 16:50:11 +0000

    main7.cpp

--- advpt/ex04/main7.cpp
+++ advpt/ex04/main7.cpp
@@ -12,5 +12,37 @@
 //
 //=================================================================================================
 
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main(int argc, char* argv[]) {
+		
+		int ia[] = {0,1,1,2,3,5,8,13,21,34,55,89};
+
+		std::vector<int> v(ia, ia+12);
+		std::list<int> l(ia, ia+12);
+
+		for (std::vector<int>::iterator it = v.begin(); it != v.end();) {
+				std::vector<int>::iterator here = it++;
+				if (*here%2==0)
+						v.erase(here);
+		}
+
+		for (std::list<int>::iterator it = l.begin(); it != l.end();) {
+				std::list<int>::iterator here = it++;
+				if (*here%2==1)
+						l.erase(here);
+		}
 
 
+		for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+				std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+
+		for (std::list<int>::iterator it = l.begin(); it != l.end(); ++it) {
+				std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+}

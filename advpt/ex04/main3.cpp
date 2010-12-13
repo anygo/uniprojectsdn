c7453117b51d9e151847b931952e62c3df49ee5b
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-12-13 16:06:37 +0000

    advpt ex04 main2 und main3

--- advpt/ex04/main3.cpp
+++ advpt/ex04/main3.cpp
@@ -11,4 +11,33 @@
 //=================================================================================================
 
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
 
+int fib(int num) {
+		if (num == 0 || num == 1)
+				return 1;
+		else 
+				return fib(num-1) + fib(num-2);
+}
+
+int main(int argc, char* argv[]) {
+
+		std::vector<std::string> fibs;
+
+		for (int i = 0; i < 10; ++i) {
+				int cur = fib(i);
+				std::stringstream ss;
+				ss << cur;
+				fibs.push_back(ss.str());
+		}
+
+		sort(fibs.begin(), fibs.end());
+
+		for (int i = 0; i < 10; ++i) {
+				std::cout << fibs[i] << std::endl;
+		}
+}

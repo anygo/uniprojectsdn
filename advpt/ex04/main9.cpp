153d2f282abfdf54e9bea9bb385b5db8fde93ccc
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2010-12-13 17:14:31 +0000

    += advpt ex4 finished

--- advpt/ex04/main9.cpp
+++ advpt/ex04/main9.cpp
@@ -14,4 +14,58 @@
 //=================================================================================================
 
 
+#include <iostream>
+#include <sstream>
+#include <set>
+#include <cstdlib>
 
+struct comp {
+		bool operator() (const unsigned int& lhs, const unsigned int& rhs) {
+				std::stringstream ss;
+				ss << lhs;
+				std::stringstream ss2;
+				ss2 << rhs;
+
+				
+				std::string l = ss.str();
+				std::string r = ss2.str();
+
+				
+				//we could have done some awesome stuff:
+				//Math.log(Math.abs(lhs)) + 1
+				//... whatever
+				if (r.size() == l.size()) {
+						return l < r;
+				} else {
+						return l.size() < r.size();
+				}
+		}
+};
+
+int main(int argc, char* argv[]) {
+		std::set<unsigned int, comp> s;
+
+		for (int i = 0; i < 15; ++i) {
+				s.insert(std::rand()%10000);
+		}	
+
+		for (std::set<unsigned int, comp>::iterator it = s.begin(); it != s.end(); ++it) {
+				std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+
+		for (std::set<unsigned int, comp>::iterator it = s.begin(); it != s.end();) {
+				std::set<unsigned int, comp>::iterator here = it++;
+				
+				std::stringstream ss;
+				ss << *here;
+				if (ss.str().c_str()[0] == '1')
+						s.erase(here);
+		}
+
+
+		for (std::set<unsigned int, comp>::iterator it = s.begin(); it != s.end(); ++it) {
+				std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+}

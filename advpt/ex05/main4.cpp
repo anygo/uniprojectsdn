506f34a010ec68cbdb9220bdf91672f11de7c56d
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-01-06 17:48:57 +0000

    crap

--- advpt/ex05/main4.cpp
+++ advpt/ex05/main4.cpp
@@ -11,5 +11,43 @@
 //
 //=================================================================================================
 
+#include <string>
+#include <list>
+#include <vector>
+#include <iostream>
+
+template<class Iterator, class T> Iterator find( Iterator start, Iterator end, const T& val) {
+	while(start != end) {
+		if (*start == val)
+				break;
+		++start;
+	}
+	return start;
+}
+
+
+int main(int argc, char* argv[]) {
+
+	std::vector<int> intvec(100,10);
+	std::list<std::string> list;
+
+	intvec[23] = 42;
+	list.push_back("Spaten");
+	list.push_back("Garten");
+	list.push_back("Mausefalle");
+	list.push_back("Hundefaust");
+	list.push_back("Garten");
+
+	std::cout << *(find(list.begin(), list.end(), "Garten")) << std::endl;
+	std::cout << *(++find(list.begin(), list.end(), "Garten")) << std::endl;
+	std::cout << *(find(list.begin(), list.end(), "Fausthund")) << std::endl;
+
+
+	std::cout << *(find(intvec.begin(), intvec.end()  , 42)) << std::endl;
+	std::cout << *(find(intvec.begin(), intvec.end()  , 23)) << std::endl;
+	std::cout << *(find(intvec.begin(), intvec.end()-1, 23)) << std::endl;
+
+}
+
 
 

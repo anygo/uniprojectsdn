2bfefdb4abd53b5a66f25a6aa73bf2825abd7094
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-01-06 17:48:57 +0000

    crap

--- advpt/ex05/main5.cpp
+++ advpt/ex05/main5.cpp
@@ -11,5 +11,91 @@
 //
 //=================================================================================================
 
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+template<class Iterator> void bubblesort(Iterator start, Iterator end) {
+
+	Iterator curr = start;
+
+	bool count_iteration = true;
+
+	int n = 2;
+
+	for (bool swapped = true; swapped && n > 1; ) {
+
+		if (count_iteration)
+			n = 0;
+
+		swapped = false;
+		for(; curr != end; ) {
+
+			if (count_iteration) 
+				n++;
+				
+			Iterator prev = curr;
+			++curr;
+			
+			if(curr == end) break;
+
+			if( *prev > *curr ) {
+				iter_swap(prev, curr);
+				swapped = true;		
+			}
+		}
+
+		curr = start;
+		--n;
+		count_iteration = false;
+	}
+}
 
 
+int main(int argc, char* argv[]) {
+	
+	std::vector<int> v;
+	v.push_back(4);
+	v.push_back(5);
+	v.push_back(0);
+	v.push_back(2);
+	v.push_back(1);
+	v.push_back(3);
+
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	bubblesort(v.begin(), v.end());
+
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+
+
+	// jetzt fuer listen
+	std::list<int> l;
+	l.push_back(4);
+	l.push_back(5);
+	l.push_back(0);
+	l.push_back(2);
+	l.push_back(1);
+	l.push_back(3);
+
+	for (std::list<int>::iterator it = l.begin(); it != l.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	bubblesort(l.begin(), l.end());
+
+	for (std::list<int>::iterator it = l.begin(); it != l.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+}

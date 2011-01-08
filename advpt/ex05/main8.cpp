985ab64e9b78151e6b3ea21459ba827f4d79b4da
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-01-08 17:26:10 +0000

    += advpt almost done.. minor problems

--- advpt/ex05/main8.cpp
+++ advpt/ex05/main8.cpp
@@ -11,4 +11,42 @@
 //=================================================================================================
 
 
+#include <vector>
+#include <list>
+#include <map>
+#include <iostream>
 
+
+template<class Iter> Iter findMostFrequentElement(Iter first, Iter last) {
+
+		std::map<Iter, int> elems;
+		for ( ; first != last; ++first) {
+				if (elems.find(first) == elems.end()) 
+						elems.insert(std::pair<Iter, int>(first, 1));
+				else
+						elems[first]++;
+		}
+
+		Iter max;
+		int max_cnt = 0;
+		std::map<Iter, int>iterator it;
+		for (it = elems.begin(); it != elems.end(); ++it) {
+				if ((*it).second > max_cnt) {
+						max_cnt = (*it).second;
+						max = (*it).first;
+				}
+		}
+
+		return max;
+}
+
+int main(int argc, char *argv[]) {
+		std::vector<int> v;
+		v.push_back(1);
+		v.push_back(2);
+		v.push_back(1);
+		v.push_back(1);
+		v.push_back(2);
+
+		//std::cout << findMostFrequentElement(v.begin(), v.end()) << std::endl;
+}

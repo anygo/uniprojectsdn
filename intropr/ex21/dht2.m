69eaa3f0de49c3129b2cc72b9402c531df6a5cb7
dominik.neumann@gmail.com <dominik.neumann@gmail.com@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-01-11 23:26:31 +0000

    += intropr wavelet-exercise

--- intropr/ex21/dht2.m
+++ intropr/ex21/dht2.m
@@ -14,43 +14,31 @@ hp = 1/sqrt(2)*[-1 1];
 sig = [signal signal(:,1)];
 
 % compute the convolution with hp and lp
-approximationH = conv2(sig,lp,'valid');
-detailH = conv2(sig,hp,'valid');
+approximation = conv2(sig,lp,'valid');
+detail = conv2(sig,hp,'valid');
 
 % downsampling by factor 2
-approximationH = approximationH(1:2:end,1:2:end);
-detailH = detailH(1:2:end,1:2:end);
+approximation = approximation(:,1:2:end);
+detail = detail(:,1:2:end);
 
 % ------------columns-------------
 % 1D dht along the columns of the approximation
 % needs periodic substitution of signal by one element (half the kernel
 % size)
 
-sig = [signal; signal(1,:)];
+approximation = [approximation; approximation(1,:)];
+detail = [detail; detail(1,:)];
 
-% compute the convolution with hp and lp
-approximationV = conv2(sig,lp','valid');
-detailV = conv2(sig,hp','valid');
+approx = conv2(approximation,lp','valid');
+approx = approx(1:2:end,:);
 
-% downsampling by factor 2
-approximationV = approximationV(1:2:end,1:2:end);
-detailV = detailV(1:2:end,1:2:end);
+detailH = conv2(approximation,hp','valid');
+detailH = detailH(1:2:end,:);
 
+detailV = conv2(detail,lp','valid');
+detailV = detailV(1:2:end,:);
 
-% in beide richtungen
-sig = [signal; signal(1,:)];
-sig = [sig sig(:,1)];
+detailD = conv2(detail,hp','valid');
+detailD = detailD(1:2:end,:);
 
-lp_diag = 1/sqrt(2)*[1 0; 0 1];
-hp_diag = 1/sqrt(2)*[-1 0; 0 1];
-
-% compute the convolution with hp and lp
-approximationD = conv2(sig,lp_diag,'valid');
-detailD = conv2(sig,hp_diag,'valid');
-
-% downsampling by factor 2
-%approximationD = approximationD(1:2:end,1:2:end);
-detailD = detailD(1:2:end,1:2:end);
-
-approx = sqrt((approximationH.^2 + approximationV.^2));
 end

fdf07b6c76755d01461640a60dd4646949cccac1
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-01-17 15:38:23 +0000

    bla

--- advpt/ex05/main8.cpp
+++ advpt/ex05/main8.cpp
@@ -17,19 +17,19 @@
 #include <iostream>
 
 
-template<class Iter, class Value> Value findMostFrequentElement(Iter first, Iter last) {
+template<class Iter> typename Iter::value_type findMostFrequentElement(Iter first, Iter last) {
 
-		std::map<Iter, int> elems;
+		std::map<typename Iter::value_type, int> elems;
 		for ( ; first != last; ++first) {
-				if (elems.find(first) == elems.end()) 
-						elems.insert(std::pair<Iter, int>(first, 1));
+				if (elems.find(*first) == elems.end()) 
+						elems.insert(std::pair<typename Iter::value_type, int>(*first, 1));
 				else
-						elems[first]++;
+						elems[*first]++;
 		}
 
-		Iter max;
+		typename Iter::value_type max;
 		int max_cnt = 0;
-		typename std::map<Iter, int>::iterator it;
+		typename std::map<typename Iter::value_type, int>::iterator it;
 		for (it = elems.begin(); it != elems.end(); ++it) {
 				if ((*it).second > max_cnt) {
 						max_cnt = (*it).second;
@@ -44,9 +44,12 @@ int main(int argc, char *argv[]) {
 		std::vector<int> v;
 		v.push_back(1);
 		v.push_back(2);
+		v.push_back(2);
+		v.push_back(2);
+		v.push_back(2);
 		v.push_back(1);
 		v.push_back(1);
 		v.push_back(2);
 
-		//std::cout << findMostFrequentElement(v.begin(), v.end()) << std::endl;
+		std::cout << findMostFrequentElement(v.begin(), v.end()) << std::endl;
 }

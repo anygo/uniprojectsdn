178777874e99f9c6adadb3422d00ef659292b1ff
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-02-22 15:50:26 +0000

    bla

---
+++ master/advpt/exam_preparation/strangeness.cpp
@@ -0,0 +1,19 @@
+class Base {
+public:
+	 virtual void Base::print( int i1, int i2 = 5 ) { cout << "Base" << i1 << ", " << i2 << endl; }
+
+};
+
+class Derived : public Base {
+public:
+	void Derived::print( int i1, int i2 = 77) {cout << "Derived:" << i1 << ", " << i2 << endl; }
+};
+
+int main()
+{
+	Base *pb = new Derived;
+	Derived *d = new Derived;
+	pb->print(5555); //output: Derived: 5555, 5
+	d->print(1111); //output: Derived: 1111, 77
+	return 0;
+}

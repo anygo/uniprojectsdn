9e7a5da3b98253d6ed2350b3f25cae4a536ec737
dominik.neumann@gmail.com <dominik.neumann@gmail.com@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-05-01 16:06:29 +0000

    += qt svn test

---
+++ misc/TextFinder/textfinder.cpp
@@ -0,0 +1,35 @@
+#include "textfinder.h"
+#include "ui_textfinder.h"
+
+TextFinder::TextFinder(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::TextFinder)
+{
+    ui->setupUi(this);
+    ui->textEdit->setPlainText(QString("ich\ndu\ner\nsie\nes\nwir\nihr\nsie"));
+}
+
+TextFinder::~TextFinder()
+{
+    delete ui;
+}
+
+
+void TextFinder::on_lineEdit_textChanged(QString searchString)
+{
+    QPalette p;
+    ui->textEdit->setPlainText(QString("ich\ndu\ner\nsie\nes\nwir\nihr\nsie"));
+
+    if (ui->textEdit->find(searchString))
+    {
+        p.setColor(QPalette::Base, Qt::green);
+
+    } else
+    {
+        p.setColor(QPalette::Base, Qt::red);
+    }
+
+    ui->lineEdit->setPalette(p);
+
+
+}

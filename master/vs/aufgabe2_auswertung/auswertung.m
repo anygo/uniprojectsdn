221c5e2bd9be44c04f4eec8c6b4c54a77feee7f5
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-06-06 15:59:32 +0000

    vs crap

--- master/vs/aufgabe2_auswertung/auswertung.m
+++ master/vs/aufgabe2_auswertung/auswertung.m
@@ -4,16 +4,19 @@ clear all;
 clc;
 
 %load data
-rmi = importdata('rmi')';
-rpc = importdata('rpc')';
+w_closing_connection = importdata('with_closing_connection')';
+wo_closing_connection = importdata('without_closing_connection')';
+java_rmi = importdata('java_rmi')';
 
 % plot data
 figure;
 hold on;
-plot(rmi(1,:), rmi(2,:), 'b--');
-plot(rpc(1,:), rpc(2,:), 'r');
-legend('Java RMI', 'RPC', 'Location', 'NorthWest');
+plot(java_rmi(1,:), java_rmi(2,:), 'red');
+plot(w_closing_connection(1,:), w_closing_connection(2,:), 'blue');
+plot(wo_closing_connection(1,:), wo_closing_connection(2,:), 'blue--');
+legend('Java RMI', 'RPC', 'RPC w/o closing connection', 'Location', 'NorthWest');
 title('Java versus us');
 xlabel('#messages');
 ylabel('time [ms]');
+grid on;
 hold off;

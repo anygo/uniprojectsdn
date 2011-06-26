78448b72f32c042f013155f93d54f3ffd2bbed25
dominik.neumann <dominik.neumann@6977f5ee-1903-a5b2-5d03-3fd7fc382973>
2011-06-26 17:17:40 +0000

    += Caytons RBC incorporated -> turned out to be far slower than our specialized code

---
+++ master/seminar/Stitching/ClosestPointFinderRBCCayton.cxx
@@ -0,0 +1,69 @@
+#include "ClosestPointFinderRBCCayton.h"
+#include <QTime>
+#include <iostream>
+
+
+void
+ClosestPointFinderRBCCayton::SetTarget(PointCoords* targetCoords, PointColors* targetColors, PointCoords* sourceCoords, PointColors* sourceColors)
+{ 
+	ClosestPointFinder::SetTarget(targetCoords, targetColors, sourceCoords, sourceColors);
+	initRBC();
+}
+
+
+
+unsigned short*
+ClosestPointFinderRBCCayton::FindClosestPoints(PointCoords* sourceCoords, PointColors* sourceColors)
+{
+	for(unsigned int i = 0; i < m_Target.r; i++)
+	{
+		m_Source.mat[IDX( i, 0, m_Source.ld )] = sourceCoords[i].x;
+		m_Source.mat[IDX( i, 1, m_Source.ld )] = sourceCoords[i].y;
+		m_Source.mat[IDX( i, 2, m_Source.ld )] = sourceCoords[i].z;
+		m_Source.mat[IDX( i, 3, m_Source.ld )] = sourceColors[i].r;
+		m_Source.mat[IDX( i, 4, m_Source.ld )] = sourceColors[i].g;
+		m_Source.mat[IDX( i, 5, m_Source.ld )] = sourceColors[i].b;
+	}
+
+
+
+	//QTime t;
+	//t.start();
+	//This finds the 32-NNs; if you are only interested in the 1-NN, use queryRBC(..) instead
+	queryRBC( m_Source, m_rbcS, nnsRBC, m_Distances );
+	//std::cout << "QueryTime() " << t.elapsed() << " ms" << std::endl;
+
+	for(int i = 0; i < m_NrOfPoints; ++i)
+	{
+		m_Indices[i] = static_cast<unsigned short>(nnsRBC[i]);
+	}
+
+	// return the indices which will then be used in the icp algorithm
+	return m_Indices;
+}
+//----------------------------------------------------------------------------
+
+void
+ClosestPointFinderRBCCayton::initRBC()
+{
+	//Build the RBC
+	//std::cout << "building the rbc..\n";
+	unint numReps = (int)sqrt(static_cast<float>(m_NrOfPoints));
+	initMat( &m_Target, m_NrOfPoints, 6 );
+	m_Target.mat = (real*)calloc( sizeOfMat(m_Target), sizeof(*(m_Target.mat)) );
+	for(unsigned int i = 0; i < m_Target.r; i++)
+	{
+		m_Target.mat[IDX( i, 0, m_Target.ld )] = m_TargetCoords[i].x;
+		m_Target.mat[IDX( i, 1, m_Target.ld )] = m_TargetCoords[i].y;
+		m_Target.mat[IDX( i, 2, m_Target.ld )] = m_TargetCoords[i].z;
+		m_Target.mat[IDX( i, 3, m_Target.ld )] = m_TargetColors[i].r;
+		m_Target.mat[IDX( i, 4, m_Target.ld )] = m_TargetColors[i].g;
+		m_Target.mat[IDX( i, 5, m_Target.ld )] = m_TargetColors[i].b;
+	}
+
+	QTime t;
+	t.start();
+	buildRBC( m_Target, &m_rbcS, numReps, numReps );
+	std::cout << "buiildRBC(): " << t.elapsed() << std::endl;
+}
+
---
+++ master/seminar/Stitching/ClosestPointFinderRBCCayton.h
@@ -0,0 +1,63 @@
+#ifndef ClosestPointFinderRBCCAYTON_H__
+#define ClosestPointFinderRBCCAYTON_H__
+
+#include "ClosestPointFinder.h"
+#include<stdlib.h>
+#include<cuda.h>
+#include<math.h>
+#include "rbc/defsRBC.h"
+#include "rbc/utils.h"
+#include "rbc/utilsgpu.h"
+#include "rbc/rbc.h"
+#include "rbc/brute.h"
+#include <iostream>
+
+
+/**	@class		ClosestPointFinderRBCCayton
+ *	@brief		RandomBallCover ClosestPointFinder on GPU (CUDA)
+ *	@author		Felix Lugauer and Dominik Neumann
+ *
+ *	@details
+ *	Class that implements the ClosestPointFinder Interface by using
+ *	the Random Ball Cover acceleration structure. Uses GPU capabilities.
+ */
+class ClosestPointFinderRBCCayton : public ClosestPointFinder
+{
+public:
+	ClosestPointFinderRBCCayton(int NrOfPoints, float nrOfRepsFactor) : ClosestPointFinder(NrOfPoints), m_NrOfRepsFactor(nrOfRepsFactor) 
+	{ 
+		nnsRBC = new unint[m_NrOfPoints];
+
+		initMat( &m_Source, m_NrOfPoints, 6 );
+		m_Source.mat = (real*)calloc( sizeOfMat(m_Source), sizeof(*(m_Source.mat)) );
+	}
+
+	~ClosestPointFinderRBCCayton() 
+	{
+		std::cout << "Desturcotr" << std::endl;
+		delete[] nnsRBC;
+		
+
+		free(m_Source.mat);
+	}
+
+	unsigned short* FindClosestPoints(PointCoords* sourceCoords, PointColors* sourceColors);
+	void SetTarget(PointCoords* targetCoords, PointColors* targetColors, PointCoords* sourceCoords, PointColors* sourceColors);
+	inline bool usesGPU() { return false; }
+
+protected:
+	void initRBC();
+
+	matrix m_Target;
+	matrix m_Source;
+	rbcStruct m_rbcS;
+	int m_NrOfReps;
+	float m_NrOfRepsFactor;
+	unint* nnsRBC;
+	real* distsRBC;
+
+};
+
+
+
+#endif // ClosestPointFinderRBCCAYTON_H__
